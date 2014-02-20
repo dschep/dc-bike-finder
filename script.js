f9688112bb57f0834cddc128d7e77958cf2af371
Daniel Schep <dschep@gmail.com>
2014-02-20 11:48:34 -0500

    factor out marker loading

--- script.js
+++ script.js
@@ -1,4 +1,33 @@
 (function() {
+    var update_markers = function() {
+        var xhr = new XMLHttpRequest();
+        xhr.onload = function(e) {
+            if (xhr.status === 200 || xhr.status === 0) {
+                var markers = L.markerClusterGroup();
+
+                $.each(xhr.responseXML.querySelectorAll('station'), function(i, node) {
+                    var marker = L.marker([
+                        node.querySelector('lat').textContent,
+                        node.querySelector('long').textContent
+                        ]);
+                    marker.bindPopup('<div>' +
+                            '<h3>' + node.querySelector('name').textContent + '</h3>' +
+                            '<p>Bikes: ' + node.querySelector('nbBikes').textContent + ' - ' +
+                            'Slots: ' + node.querySelector('nbEmptyDocks').textContent + '</p>' +
+                            '</div>');
+                    markers.addLayer(marker);
+                });
+
+                map.addLayer(markers);
+            } else {
+                alert('Failed to retrieve data from Capital Bikeshare')
+            }
+        }
+        xhr.open('GET', '/data/stations/bikeStations.xml', true);
+        xhr.responseType = 'document';
+        xhr.send();
+    };
+
     var map = L.map('map').setView([38.91, -77.04], 11);
 
     L.tileLayer('http://{s}.tile.opencyclemap.org/cycle/{z}/{x}/{y}.png', {
@@ -7,30 +36,5 @@
 
     L.control.locate().addTo(map);
 
-    var xhr = new XMLHttpRequest();
-    xhr.onload = function(e) {
-        if (xhr.status === 200 || xhr.status === 0) {
-            var markers = L.markerClusterGroup();
-
-            $.each(xhr.responseXML.querySelectorAll('station'), function(i, node) {
-                var marker = L.marker([
-                    node.querySelector('lat').textContent,
-                    node.querySelector('long').textContent
-                    ]);
-                marker.bindPopup('<div>' +
-                        '<h3>' + node.querySelector('name').textContent + '</h3>' +
-                        '<p>Bikes: ' + node.querySelector('nbBikes').textContent + ' - ' +
-                        'Slots: ' + node.querySelector('nbEmptyDocks').textContent + '</p>' +
-                        '</div>');
-                markers.addLayer(marker);
-            });
-
-            map.addLayer(markers);
-        } else {
-            alert('Failed to retrieve data from Capital Bikeshare')
-        }
-    }
-    xhr.open('GET', '/data/stations/bikeStations.xml', true);
-    xhr.responseType = 'document';
-    xhr.send();
+    update_markers();
 })()
